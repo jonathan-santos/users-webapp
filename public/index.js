3c74d79bd295e8776bd3ca86390c64bbc9b9ec47
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-10 15:48:32 -0300

    Updated website with code to delete and edit users

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const table = document.querySelector('table')
+
 const getUsers = async () => {
   try {
     const res = await fetch('/api/users')
@@ -11,6 +12,22 @@ const getUsers = async () => {
   }
 }
 
+const deleteUser = async (user, button) => {
+  try {
+    const res = await fetch('/api/users/' + user.id, {
+      method: 'DELETE'
+    })
+
+    if (!res.ok) {
+      throw new Error
+    }
+    
+    button.parentElement.parentElement.remove()
+  } catch (error) {
+    alert('Could not delete user')
+  }
+}
+
 const createTableRow = (user) => {
   const newRow = document.createElement('tr')
 
@@ -18,13 +35,40 @@ const createTableRow = (user) => {
     const newData = createTableData(user[prop])
     newRow.appendChild(newData)
   }
-  
+
+  newRow.appendChild(createEditLink(user))
+  newRow.appendChild(createDeleteButton(user))
+
   table.appendChild(newRow)
 }
 
 const createTableData = (value) => {
   const newData = document.createElement('td')
   newData.innerHTML = value
+  return newData
+}
+
+const createEditLink = (user) => {
+  const newData = document.createElement('td')
+
+  const newAnchor = document.createElement('a')
+  newAnchor.innerHTML = 'edit'
+  newAnchor.classList.add('edit')
+  newAnchor.href = '/user.html?id=' + user.id
+
+  newData.appendChild(newAnchor)
+  return newData
+}
+
+const createDeleteButton = (user) => {
+  const newData = document.createElement('td')
+
+  const newButton = document.createElement('button')
+  newButton.innerHTML = 'delete'
+  newButton.classList.add('delete')
+  newButton.addEventListener('click', (e) => deleteUser(user, e.target))
+
+  newData.appendChild(newButton)
   return newData
 }
 
---
+++ public/user.js
@@ -0,0 +1,89 @@
+let userId
+
+const onPageLoad = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  userId = urlParams.get('id')
+  
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    saveUser()
+  })
+
+  if (userId) {
+    loadUser()
+  }
+}
+
+const loadUser = async () => {
+  try {
+    const res = await fetch('/api/users/' + userId)
+
+    if (!res.ok) {
+      throw new Error
+    }
+
+    const user = await res.json(res)
+    document.querySelector('h1').innerHTML = user.name
+    document.querySelector('button').innerHTML = 'Update'
+    document.querySelector('#name').value = user.name
+    document.querySelector('#age').value = user.age
+  } catch (error) {
+    alert('Could not load user')
+  }
+}
+
+const saveUser = async () => {
+  const user = {
+    name: document.querySelector('#name').value,
+    age: document.querySelector('#age').value
+  }
+
+  try {
+    if (userId) {
+      await updateUser()
+    } else {
+      await saveNewUser(user)
+    }
+  } catch (error) {
+    const action = userId ? 'update' : 'save'
+    alert(`Could not ${action} the user`)
+  }
+}
+
+const updateUser = async (user) => {
+  const res = await fetch('/api/users/' + userId, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+
+  if (!res.ok) {
+    throw new Error
+  }
+
+  alert('User successfully updated')
+}
+
+
+const saveNewUser = async (user) => {
+  const res = await fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+
+  if (!res.ok) {
+    throw new Error
+  }
+
+  window.location.href = './'
+}
+
+onPageLoad()
