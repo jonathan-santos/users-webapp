744b1a0e9efac561c466a22e95ab3213539fc57b
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-10 11:54:13 -0300

    Changed default server PORT created table in webpage using the server data

--- public/script.js
+++ public/script.js
@@ -1,1 +1,33 @@
-console.log('js working')
+const table = document.querySelector('table')
+const URL = 'http://localhost:3333'
+
+const getUsers = async () => {
+  try {
+    const res = await fetch(URL + '/api/users')
+    const users = await res.json()
+    
+    users.forEach(user => createTableRow(user))
+  }
+  catch (error) {
+    alert('Could not get users...')
+  }
+}
+
+const createTableRow = (user) => {
+  const newRow = document.createElement('tr')
+
+  for (prop in user) {
+    const newData = createTableData(user[prop])
+    newRow.appendChild(newData)
+  }
+  
+  table.appendChild(newRow)
+}
+
+const createTableData = (value) => {
+  const newData = document.createElement('td')
+  newData.innerHTML = value
+  return newData
+}
+
+getUsers()
--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(routes)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3333
 app.listen(PORT, (req, res) => {
 	console.log(`Server running on port ${PORT}`)
 })
