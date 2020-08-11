d20c7bd2e52dc2e3b13b8a63386b979ba8b024f8
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-11 16:30:29 -0300

    Reverted folder name vanillaWebPage to public

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static('vanillaWebPage'))
+app.use(express.static('public'))
 app.use(routes)
 
 const PORT = process.env.PORT || 3333
